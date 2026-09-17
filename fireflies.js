f63d0861adf50616a3167150e3f08179d72c20b4
MK <148148795+mkiyah-b@users.noreply.github.com>
2026-09-17 19:50:17 -0400

    Create fireflies.js

---
+++ fireflies.js
@@ -0,0 +1,44 @@
+// character count for typewriter
+const headText = document.querySelector("#header");
+if(headText) {
+    const headWordCount = headText.textContent.length;
+    console.log(headWordCount);
+    headText.style.cssText = "--n: " + headWordCount;
+    // console.log(headText.style.cssText);
+}
+else {
+    console.log("Header Error");
+}
+
+
+const paraText = document.querySelector("#paragraph");
+if(paraText) {
+    const paraWordCount = paraText.textContent.length;
+    console.log(paraWordCount);
+    paraText.style.cssText = "--n: " + paraWordCount;
+    // console.log(paraText.style.cssText);
+}
+else {
+    console.log("Paragraph Error");
+}
+
+   
+// move the fireflies
+    const jarPos = document.querySelector("#tallJar");
+    console.log(jarPos.y);
+    const fireflies = document.querySelectorAll("#ff1");
+    fireflies.forEach(firefly => {
+        let randomHeight = (Math.random() * 500) + jarPos.y + 200; 
+        // console.log(randomHeight);
+        firefly.style.top = randomHeight + "px";
+        
+        let randomWidth = (Math.random() * 300) + jarPos.x + 300; 
+        // console.log(randomWidth);
+        firefly.style.left = randomWidth + "px";
+        
+        let randomLength = (Math.random() * 35) + 20;
+        // console.log(randomLength);
+        firefly.style.width = randomLength + "px";
+    })
+    
+
